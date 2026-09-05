56a5b4db4ba18a97918642e78c79fda2e4320ce8
NarutoUchiha39 <104666748+NarutoUchiha39@users.noreply.github.com>
2026-09-05 11:17:01 +0530

    Add: boats-to-save-people - submission-2

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+
+        l = 0
+        r = len(people)-1
+        res = 0
+        people.sort()
+        while(l<=r):
+            remain = limit - people[r]
+            res+= 1
+            r -= 1
+
+            if(l<=r and remain >= people[l]):
+                l+=1
+        
+        return res
